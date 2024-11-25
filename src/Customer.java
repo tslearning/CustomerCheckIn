6d5a5077662a8ce3cd5208e3c6a611ecc374f3d5
Touyer Lee <touyer.lees@gmail.com>
2024-11-25 14:54:21 -0600

    Checkin

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Customer {
+
+    String[] customer = new String[5];
+    Scanner input = new Scanner(System.in);
+    String firstName="";
+    String lastName="";
+
+    public void  inputCustomerData() {
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Enter the first name of the customer %d: ", i + 1);
+            while (true) {
+                firstName = input.nextLine().trim();
+                if  (!firstName.isEmpty()) break;
+                System.out.println("Firstname cannot be empty. Please try again.:");
+                System.out.printf("Enter the first name of the customer %d: ", i + 1);
+            }
+            System.out.printf("Enter the last name of the customer %d: ", i + 1);
+
+            while (true) {
+                lastName = input.nextLine().trim();
+                if  (!lastName.isEmpty()) break;
+                System.out.println("Lastname cannot be empty. Please try again.:");
+                System.out.printf("Enter the last name of the customer %d: ", i + 1);
+            }
+            customer[i] = firstName + " " + lastName;
+        }
+    }
+    public void displayCustomerData() {
+        for(String name:customer){
+            System.out.println(name);
+        }
+    }
 }
